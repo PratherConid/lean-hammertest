a748735328ac15f02415b3406c11fe79df422798
PratherConid <syxx2013050321@163.com>
2025-01-06 01:36:30 -0800

    update

--- TestAuto/evaluate_mathlib.lean
+++ TestAuto/evaluate_mathlib.lean
@@ -1,71 +1,25 @@
 import Mathlib
-import Hammertest.DuperInterface
-import Auto.EvaluateAuto.TestAuto
 import Auto.EvaluateAuto.TestTactics
 
-open Lean Meta Elab Auto EvalAuto
+open EvalAuto
 
-attribute [rebind Auto.Native.solverFunc] Auto.duperRaw
-set_option trace.auto.tactic true
---set_option trace.auto.runAuto.printLemmas true
-set_option trace.auto.eval.printResult true
---set_option trace.auto.tptp.printProof true
+-- #eval evalTacticsAtMathlibHumanTheorems
+--   { tactics := #[.useRfl, .useSimpAll, .useSimpAllWithPremises, .useAesop 4096, .useAesopWithPremises 4096], resultFolder := "./Eval",
+--     nonterminates := #[], nthreads := 4 }
 
--- #eval @id (CoreM _) do
---   let all ← allHumanTheorems
---   let n := 512
---   NameArray.save (← Array.randPick all n) s!"MathlibNames{n}.txt"
-
--- set_option maxHeartbeats 200000000
--- #eval runAutoOnNamesFile
---   { solverConfig := .tptp (.zeport .lams) "/home/indprinciples/Programs/zipperposition/portfolio",
---     logFile := "evalAutoOut.txt" }
---   "EvalResults/MathlibNames128.txt"
-
--- set_option auto.mono.ignoreNonQuasiHigherOrder true
--- set_option maxHeartbeats 200000000
--- #eval runAutoOnNamesFile
---   { solverConfig := .native, maxHeartbeats := 65536,
---     logFile := "evalAutoOut.txt" }
---   "EvalResults/MathlibNames128.txt"
-
--- set_option maxHeartbeats 200000000
--- #eval runAutoOnNamesFile
---   { solverConfig := .smt .z3,
---     logFile := "evalAutoOut.txt" }
---   "EvalResults/MathlibNames128.txt"
-
-set_option trace.auto.mono true
-set_option trace.auto.lamReif.printResult true
-
--- Incompleteness
-#check WeierstrassCurve.Affine.Point.map_id
-#check RingHom.FiniteType.of_finite
-#check List.map_concat
-#check SuccOrder.nhdsWithin_Ici
-#check isLUB_sSup
-#check Set.image_preimage
-#check WCovBy.le_of_lt
-
-set_option auto.evalAuto.ensureAesop true
-
--- open EvalAuto in
--- #eval @id (CoreM _) do
---   let p ← initSrcSearchPath
---   let r ← runTacticsAtConstantDeclaration ``UInt32.toUInt16_toNat p
---     #[fun _ => useSimpAll, useSimpAllWithPremises, fun _ => useAesop, useAesopWithPremises]
---   trace[auto.tactic] "{r}"
-
--- open EvalAuto in
--- #eval @id (CoreM _) do
---   let p ← initSrcSearchPath
---   let r ← evalAtModule `Mathlib.RingTheory.FiniteType p (fun ci => ci.name == ``AddMonoidAlgebra.mem_adjoin_support)
---     { tactics := #[.useRfl, .useSimp, .useAesop, .useAesopWithPremises], logFile := "evalTacticOut.txt" }
---   trace[auto.tactic] "{r}"
-
-#check AddMonoidAlgebra.mem_adjoin_support
-
--- set_option maxHeartbeats 2000
--- theorem mem_adjoin_support.{u_1, u_2} {R : Type u_1} {M : Type u_2} [CommSemiring R] [AddMonoid M]
---   (f : AddMonoidAlgebra R M) : f ∈ Algebra.adjoin R (AddMonoidAlgebra.of' R M '' ↑f.support) := by
---   aesop
+-- #eval do
+--   let r ← readTacticEvalResult
+--     { tactics := #[.useRfl, .useSimpAll, .useSimpAllWithPremises, .useAesop 4096, .useAesopWithPremises 4096], resultFolder := "./Eval",
+--       nonterminates := #[], nthreads := 4 }
+--   let r := (r.map Prod.snd).flatMap id
+--   let r := r.map Prod.snd
+--   IO.println s!"Total : {r.size}"
+--   for idx in [0:5] do
+--     let t := r.map (fun r => match r[idx]! with | Result.success => 1 | _ => 0)
+--     let t := t.foldl Nat.add 0
+--     IO.println s!"{idx} : {t}"
+--   let cumulative : Array Bool := r.map (fun s => Array.any s (
+--      fun r => match r with | Result.success => true | _ => false))
+--   let cumulative := cumulative.map (fun b : Bool => if b then 1 else 0)
+--   let t := cumulative.foldl Nat.add 0
+--   IO.println s!"cul : {t}"
---
+++ TestAuto/evaluate_test.lean
@@ -0,0 +1,71 @@
+import Mathlib
+import Hammertest.DuperInterface
+import Auto.EvaluateAuto.TestAuto
+import Auto.EvaluateAuto.TestTactics
+
+open Lean Meta Elab Auto EvalAuto
+
+attribute [rebind Auto.Native.solverFunc] Auto.duperRaw
+set_option trace.auto.tactic true
+--set_option trace.auto.runAuto.printLemmas true
+set_option trace.auto.eval.printResult true
+--set_option trace.auto.tptp.printProof true
+
+-- #eval @id (CoreM _) do
+--   let all ← allHumanTheorems
+--   let n := 512
+--   NameArray.save (← Array.randPick all n) s!"MathlibNames{n}.txt"
+
+-- set_option maxHeartbeats 200000000
+-- #eval runAutoOnNamesFile
+--   { solverConfig := .tptp (.zeport .lams) "/home/indprinciples/Programs/zipperposition/portfolio",
+--     logFile := "evalAutoLog.txt", resultFile := "evalAutoResult.txt" }
+--   "EvalResults/MathlibNames128.txt"
+
+-- set_option auto.mono.ignoreNonQuasiHigherOrder true
+-- set_option maxHeartbeats 200000000
+-- #eval runAutoOnNamesFile
+--   { solverConfig := .native, maxHeartbeats := 65536,
+--     logFile := "evalAutoLog.txt", resultFile := "evalAutoResult.txt" }
+--   "EvalResults/MathlibNames128.txt"
+
+-- set_option maxHeartbeats 200000000
+-- #eval runAutoOnNamesFile
+--   { solverConfig := .smt .z3,
+--     logFile := "evalAutoLog.txt", resultFile := "evalAutoResult.txt" }
+--   "EvalResults/MathlibNames128.txt"
+
+set_option trace.auto.mono true
+set_option trace.auto.lamReif.printResult true
+
+-- Incompleteness
+#check WeierstrassCurve.Affine.Point.map_id
+#check RingHom.FiniteType.of_finite
+#check List.map_concat
+#check SuccOrder.nhdsWithin_Ici
+#check isLUB_sSup
+#check Set.image_preimage
+#check WCovBy.le_of_lt
+
+set_option auto.evalAuto.ensureAesop true
+
+-- #check @id (CoreM _) do
+--   let p ← initSrcSearchPath
+--   let r ← runTacticsAtConstantDeclaration ``UInt32.toUInt16_toNat p
+--     #[fun _ => useSimpAll, useSimpAllWithPremises, fun _ => useAesop 4096, useAesopWithPremises 4096]
+--   trace[auto.tactic] "{r}"
+
+-- #eval @id (CoreM _) do
+--   let p ← initSrcSearchPath
+--   let m := Std.HashSet.ofList (← allHumanTheorems).toList
+--   let r ← evalAtModule `Mathlib.Algebra.Group.Defs p (fun ci => m.contains ci.name)
+--     { tactics := #[.useRfl, .useSimpAll, .useSimpAllWithPremises, .useAesop 4096, .useAesopWithPremises 4096],
+--       logFile := "evalTacticLog.txt", resultFile := "evalTacticResult.txt"
+--       nonterminates := #[] }
+--   trace[auto.tactic] "{r}"
+
+-- #eval evalTacticsAtMathlibHumanTheorems
+--   { tactics := #[.useRfl, .useSimpAll, .useSimpAllWithPremises, .useAesop 4096, .useAesopWithPremises 4096], resultFolder := "./Eval",
+--     nonterminates := #[], nthreads := 8 }
+
+#check neg_eq_of_add_eq_zero_right
