985274ee18560d74c4b414128d48dce9ac4bc2ae
PratherConid <syxx2013050321@163.com>
2025-01-11 11:48:57 -0800

    more threads

--- setup_env_run_test.sh
+++ setup_env_run_test.sh
@@ -5,7 +5,7 @@ bash elan-init.sh -y
 rm elan-init.sh
 git clone https://github.com/leanprover-community/lean-auto
 # TODO: Use up-to-date version
-cd lean-auto; git checkout 26d9cb3da795010b3e4e2110e155e8b13ed2c2c0; cd ..
+cd lean-auto; git checkout 384d3081a9ae0eb01cdf8765480060e1a11c75a3; cd ..
 git clone https://github.com/leanprover-community/duper
 cd duper; git checkout 9cd4d4d1d71034d456d06aef2e4d07c911b88c65; cd ..
 git clone https://github.com/PratherConid/lean-hammertest
@@ -21,7 +21,7 @@ open EvalAuto
 #eval evalAutoAtMathlibHumanTheorems
   { maxHeartbeats := 65536, timeout := 10, solverConfig := .native,
     resultFolder  := \"./EvalAuto\",
-    nthreads := 8, batchSize := 512 }" | lake env lean --stdin
+    nthreads := 32, batchSize := 512 }" | lake env lean --stdin
 
 echo "import Mathlib
 import Auto.EvaluateAuto.TestTactics
@@ -31,4 +31,4 @@ open EvalAuto
 #eval evalTacticsAtMathlibHumanTheorems
   { tactics := #[.useRfl, .useSimpAll, .useSimpAllWithPremises, .useAesop 16384, .useAesopWithPremises 16384],
     resultFolder := "./EvalTactics",
-    nonterminates := #[], nthreads := 8 }" | lake env lean --stdin
+    nonterminates := #[], nthreads := 32 }" | lake env lean --stdin
