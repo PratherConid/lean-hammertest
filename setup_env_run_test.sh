d8e8e81ecaf741de63f78f244b2f2148c983879a
PratherConid <syxx2013050321@163.com>
2025-01-12 01:39:13 -0800

    update lean-auto

--- setup_env_run_test.sh
+++ setup_env_run_test.sh
@@ -5,7 +5,7 @@ bash elan-init.sh -y
 rm elan-init.sh
 git clone https://github.com/leanprover-community/lean-auto
 # TODO: Use up-to-date version
-cd lean-auto; git checkout 8650ab97adf33e2d69754189e329f001d5c41a66; cd ..
+cd lean-auto; git checkout 1c67721a3e218bc9337550384ef5d880a3de9315; cd ..
 git clone https://github.com/leanprover-community/duper
 cd duper; git checkout 9cd4d4d1d71034d456d06aef2e4d07c911b88c65; cd ..
 git clone https://github.com/PratherConid/lean-hammertest
